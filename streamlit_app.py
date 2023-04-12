fc681e4ac164c1359b56cf2cf937e8669a99a907
Aaron Mendelson <aaron.mendelson@snowflake.com>
2023-04-12 16:54:47 -0700

    Update streamlit_app.py
    fix json

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
-import requests 
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
