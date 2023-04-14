aa2c89d417b0223e5e7565eb23e14448815d2feb
Aaron Mendelson <aaron.mendelson@snowflake.com>
2023-04-14 14:03:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
           streamlit.error("please select a fruit to get information.")
    else: 
         back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.data_frame(back_from_function)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
       
